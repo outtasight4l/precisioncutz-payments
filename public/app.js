e8a8fb2953335f149ff24459bb5d7049f10639de
outtasight4l <outtasight4l@gmail.com>
2026-05-26 08:10:08 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
+let amount = "";
 
+function addNumber(num) {
+  if (num === "." && amount.includes(".")) return;
+
+  amount += num;
+  updateDisplay();
+}
+
+function clearAmount() {
+  amount = amount.slice(0, -1);
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("amount").innerText =
+    amount === "" ? "0.00" : parseFloat(amount).toFixed(2);
+}
