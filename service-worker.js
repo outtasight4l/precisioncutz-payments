1f6959ace458dba308b5a4896a6d4bf050369a97
outtasight4l <outtasight4l@gmail.com>
2026-05-26 21:12:10 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
